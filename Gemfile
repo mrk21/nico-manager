d4ebd864867e6eb23e70c356059cb8aad6f4ea46
Yuichi Murata <mrk21info+github@gmail.com>
2015-03-03 15:11:02 +0900

    Added the project files.

---
+++ Gemfile
@@ -0,0 +1,30 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.2.0'
+gem 'pg'
+gem 'jbuilder', '~> 2.0'
+gem 'sdoc', '~> 0.4.0', group: :doc
+
+# Use ActiveModel has_secure_password
+# gem 'bcrypt', '~> 3.1.7'
+
+# Use Unicorn as the app server
+# gem 'unicorn'
+
+# Use Capistrano for deployment
+# gem 'capistrano-rails', group: :development
+
+group :development, :test do
+  gem 'byebug'
+  gem 'web-console', '~> 2.0'
+  
+  gem 'guard-rspec'
+  gem 'spring'
+  gem 'spring-commands-rspec'
+  
+  gem 'rspec-rails', '~> 3.0'
+  gem 'factory_girl_rails'
+  gem 'rspec-request_describer'
+  gem 'json_spec'
+  gem 'database_rewinder'
+end
---
+++ config/application.rb
@@ -0,0 +1,27 @@
+require File.expand_path('../boot', __FILE__)
+
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+
+Bundler.require(*Rails.groups)
+
+module NicoManager
+  class Application < Rails::Application
+    config.active_record.raise_in_transactional_callbacks = true
+    
+    config.generators do |g|
+      g.javascripts false
+      g.stylesheets false
+      g.helper false
+      g.template_engine :jbuilder
+      g.test_framework :rspec,
+        view_specs: false,
+        controller_specs: false,
+        routing_specs: false
+    end
+  end
+end
