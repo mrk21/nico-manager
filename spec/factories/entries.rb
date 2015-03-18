a7acd48a5b23c1e8e4c9155bac975960a2c033be
Yuichi Murata <mrk21info+github@gmail.com>
2015-03-18 22:07:55 +0900

    Fixed test data.

--- spec/factories/entries.rb
+++ spec/factories/entries.rb
@@ -2,10 +2,20 @@ FactoryGirl.define do
   factory :entry do
     association :mylist, factory: :mylist
     association :video, factory: :video
+    
+    item_type 0
+    item_id 1407498125
+    created_time Time.at(1409129700)
+    updated_time Time.at(1409129700)
   end
   
   factory :entry_without_user_creation, class: Entry do
     association :mylist, factory: :mylist_without_user_creation
     association :video, factory: :video
+    
+    item_type 0
+    item_id 1407498125
+    created_time Time.at(1409129700)
+    updated_time Time.at(1409129700)
   end
 end
--- spec/factories/videos.rb
+++ spec/factories/videos.rb
@@ -3,5 +3,15 @@ FactoryGirl.define do
     video_id "sm24189468"
     title "自主制作アニメ【鬼斬娘】 ᵗˢᵘᵇᵘʳᵃ"
     thumbnail_url "http://tn-skr1.smilevideo.jp/smile?i=24189468"
+    group_type "default"
+    watch_id "sm24189468"
+    is_deleting 0
+    created_time Time.at(1407498125)
+    updated_time Time.at(1425803139)
+    play_count 504953
+    mylist_count 28846
+    comment_count 6072
+    seconds 131
+    latest_comment "!? すげぇwww 東京喰種にこれに似た 悪口は最後まで見てか 後半見ても文... "
   end
 end
