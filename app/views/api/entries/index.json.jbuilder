c16e28a375dc6df071e72cf7d582d48ee7678987
Yuichi Murata <mrk21info+github@gmail.com>
2015-03-18 23:19:58 +0900

    Changed the entries API to return some items.

--- app/views/api/entries/index.json.jbuilder
+++ app/views/api/entries/index.json.jbuilder
@@ -1,3 +1,30 @@
 json.array!(@entries) do |entry|
-  json.extract! entry.video, :video_id, :title, :thumbnail_url
+  json.extract!(entry.mylist, :group_id)
+  
+  json.extract!(entry,
+    :item_type,
+    :item_id,
+    :created_time,
+    :description
+  )
+  
+  json.video do
+    json.extract!(entry.video,
+      :video_id,
+      :title,
+      :thumbnail_url,
+      :video_id,
+      :title,
+      :thumbnail_url,
+      :group_type,
+      :watch_id,
+      :is_deleting,
+      :created_time,
+      :play_count,
+      :mylist_count,
+      :comment_count,
+      :seconds,
+      :latest_comment
+    )
+  end
 end
--- spec/requests/api/api_entries_spec.rb
+++ spec/requests/api/api_entries_spec.rb
@@ -10,9 +10,29 @@ RSpec.describe "Api::Entries", type: :request do
       it 'should be all entries of the authenticated user' do
         is_expected.to eq 200
         video = self.entry.video
-        expect(body).to be_json_eql(video.video_id.to_json).at_path('0/video_id')
-        expect(body).to be_json_eql(video.title.to_json).at_path('0/title')
-        expect(body).to be_json_eql(video.thumbnail_url.to_json).at_path('0/thumbnail_url')
+        
+        # mylist
+        expect(body).to be_json_eql(entry.mylist.group_id .to_json).at_path '0/group_id'
+        
+        # entry
+        expect(body).to be_json_eql(entry.item_type       .to_json).at_path '0/item_type'
+        expect(body).to be_json_eql(entry.item_id         .to_json).at_path '0/item_id'
+        expect(body).to be_json_eql(entry.created_time    .to_json).at_path '0/created_time'
+        expect(body).to be_json_eql(entry.description     .to_json).at_path '0/description'
+        
+        # video
+        expect(body).to be_json_eql(video.video_id        .to_json).at_path '0/video/video_id'
+        expect(body).to be_json_eql(video.watch_id        .to_json).at_path '0/video/watch_id'
+        expect(body).to be_json_eql(video.title           .to_json).at_path '0/video/title'
+        expect(body).to be_json_eql(video.thumbnail_url   .to_json).at_path '0/video/thumbnail_url'
+        expect(body).to be_json_eql(video.play_count      .to_json).at_path '0/video/play_count'
+        expect(body).to be_json_eql(video.mylist_count    .to_json).at_path '0/video/mylist_count'
+        expect(body).to be_json_eql(video.comment_count   .to_json).at_path '0/video/comment_count'
+        expect(body).to be_json_eql(video.seconds         .to_json).at_path '0/video/seconds'
+        expect(body).to be_json_eql(video.latest_comment  .to_json).at_path '0/video/latest_comment'
+        expect(body).to be_json_eql(video.group_type      .to_json).at_path '0/video/group_type'
+        expect(body).to be_json_eql(video.is_deleting     .to_json).at_path '0/video/is_deleting'
+        expect(body).to be_json_eql(video.created_time    .to_json).at_path '0/video/created_time'
       end
     end
   end
