0e3467dc3a9031995f6a0e3c2f1a325c0bbb5a25
Yuichi Murata <mrk21info+github@gmail.com>
2015-03-23 16:17:15 +0900

    Added the search scope to the Entry.

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -3,5 +3,20 @@ class Entry < ActiveRecord::Base
   belongs_to :video
   accepts_nested_attributes_for :video
   
-  scope :tagged_with, ->(*args){ joins(:video).merge(Video.tagged_with(*args)) }
+  scope :tagged_with, ->(*args){
+    joins(:video).merge(Video.tagged_with(*args))
+  }
+  
+  scope :search, ->(words){
+    words = words.split(/\s+/)
+    
+    from("%s entries" % 
+      words.reduce(self){|r, word|
+        r.where(Video.arel_table[:title].matches("%#{word}%"))
+      }
+      .joins(:video)
+      .union(tagged_with(words))
+      .to_sql
+    )
+  }
 end
--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -1,4 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Entry, type: :model do
+  describe '#search(words)' do
+    before do
+      FactoryGirl.create(:user_template, mylists_params: [
+        entries_params: [
+          {video_params: {video_id: 'sm1', title: 'hoge'    , tag_list: ['tag1','tag2']}},
+          {video_params: {video_id: 'sm2', title: 'foo'     , tag_list: ['tag3'       ]}},
+          {video_params: {video_id: 'sm3', title: 'hoge_foo', tag_list: ['tag3','tag2']}},
+          {video_params: {video_id: 'sm4', title: 'bar'     , tag_list: ['hoge','tag2']}},
+        ]
+      ])
+    end
+    
+    let(:search_words){'hoge'}
+    let(:expected){['sm1','sm3','sm4']}
+    
+    subject { Entry.search(self.search_words).map{|r| r.video.video_id}.sort }
+    
+    it 'should be entries whose title contains the search words or tag matches to the search words' do
+      is_expected.to eq self.expected
+    end
+    
+    context 'when the words was multiple' do
+      let(:search_words){'hoge foo'}
+      let(:expected){['sm3']}
+      
+      it 'should be entries matched each word' do
+        is_expected.to eq self.expected
+      end
+    end
+  end
 end
