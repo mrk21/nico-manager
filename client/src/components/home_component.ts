9516b8dd96e7205ffb4b8f6c386911d45212b738
Yuichi Murata <mrk21info+github@gmail.com>
2015-04-10 23:59:27 +0900

    Added pagination to the home component.

--- client/src/components/home_component.ts
+++ client/src/components/home_component.ts
@@ -1,16 +1,21 @@
 import React = require("react");
 import Router = require('react-router');
 import Base = require('./base');
+
 import EntryStore = require('../stores/entry_store');
 import MylistStore = require('../stores/mylist_store');
 import TagStore = require('../stores/tag_store');
+
 import SearchFormComponent = require('../components/search_form_component');
 import TagListComponent = require('../components/tag_list_component');
 import MylistListComponent = require('../components/mylist_list_component');
+import PaginationComponent = require('../components/pagination_component');
+
 import Link = Router.Link;
 import SearchForm = SearchFormComponent.Component;
 import TagList = TagListComponent.Component;
 import MylistList = MylistListComponent.Component;
+import Pagination = PaginationComponent.Component;
 
 export interface Props extends Base.Props {}
 export interface State extends Base.State {
@@ -45,6 +50,11 @@ export class Spec extends Base.Spec<Props, State> {
     updateEntries() {
         var params: any = this.getParams() || {};
         var query: any = this.getQuery() || {};
+        var currentPage: number = query.p || 1;
+        var range: Api.Range = {
+            since: 20 * (currentPage - 1),
+            until: 20 * (currentPage),
+        };
         
         if (this.isActive('mylist_entries')) {
             this.getFlux().actions.mylist.entry(params.group_id, query.q);
@@ -53,7 +63,7 @@ export class Spec extends Base.Spec<Props, State> {
             this.getFlux().actions.tag.entry(params.name, query.q);
         }
         else {
-            this.getFlux().actions.entry.index(query.q);
+            this.getFlux().actions.entry.index(query.q, range);
         }
     }
     
@@ -63,6 +73,26 @@ export class Spec extends Base.Spec<Props, State> {
         if (!this.state.entry.isFetched) {
             return React.jsx(`<article />`);
         }
+        
+        var query: any = this.getQuery() || {};
+        var currentPage: number = query.p || 1;
+        var paginationProps: PaginationComponent.Props = {
+            flux: this.getFlux(),
+            total: this.state.entry.range.total,
+            perPage: 20,
+            currentPage: currentPage,
+            getLinkProps: (page: number) => {
+                var query: any = this.getQuery() || {};
+                query.p = page;
+                
+                return {
+                    to: this.getPathname(),
+                    params: this.getParams(),
+                    query: query
+                };
+            }
+        };
+        
         return React.jsx(`
             <article className="l-entry">
                 <div className="l-entry__nav">
@@ -80,6 +110,8 @@ export class Spec extends Base.Spec<Props, State> {
                         <SearchForm ref="searchForm" />
                     </div>
                     
+                    <Pagination {...paginationProps} />
+                    
                     <ul className="c-entry-list" ref="entryList">{this.state.entry.list.map((entry) =>
                         <li className="c-entry-list__list-item" key={[entry.mylist.group_id, entry.entry.item_id].join('-')} title={entry.video.title}>
                             <a href={'http://www.nicovideo.jp/watch/' + entry.video.watch_id}>
@@ -92,6 +124,8 @@ export class Spec extends Base.Spec<Props, State> {
                             </div>
                         </li>
                     )}</ul>
+                    
+                    <Pagination {...paginationProps} />
                 </section>
             </article>
         `);
